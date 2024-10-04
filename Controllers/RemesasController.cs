d32c52e78bb6b1b9193ee00f258fddba9e24f1c4
EkouNaylamp <jpea99lr@gmail.com>
2024-10-04 18:53:47 -0500

    V1.0

---
+++ Controllers/RemesasController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2024.Controllers
+{
+    public class RemesasController
+    {
+        
+    }
+}
